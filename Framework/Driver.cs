ed295d7545b415c049e035bd3974e6a229101ef9
PEdita <Editap87@gmail.com>
2023-04-01 15:12:44 +0300

    Add Driver class

---
+++ Framework/Driver.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Framework
+{
+    public class Driver
+    {
+        private static IWebDriver driver;
+
+        public static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        public static void Initialize()
+        {
+            ChromeOptions options = new ChromeOptions();
+            driver = new ChromeDriver(options);
+        }
+
+        public static void CloseDriver()
+        {
+            driver.Quit();
+        }
+
+        public static void OpenPage(string url)
+        {
+            driver.Url = url;
+        }
+    }
+}
