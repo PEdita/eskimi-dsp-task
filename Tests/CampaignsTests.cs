1e0de8680cdd97ef3b4a38764a900e3f18652b2f
PEdita <Editap87@gmail.com>
2023-04-04 14:45:05 +0300

    Added test in CampaignsTests class

--- Tests/CampaignsTests.cs
+++ Tests/CampaignsTests.cs
@@ -1,6 +1,42 @@
-﻿namespace Tests
+﻿using Framework.EskimiDspPOM;
+using Framework;
+using NUnit.Framework;
+
+namespace Tests
 {
-    internal class CampaignsTests
+    public class CampaignsTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Initialize();
+            Campaigns.Open();
+        }
+
+        [Test] 
+        public void CreateNewCampaign()
+        {
+            string expectedCampaignName = "Pasaulis";
+            string expectedStatus = "Dra.";
+
+            Campaigns.ClickNewCampaignButton();
+            Campaigns.ClickCreativeTypeDropdown();
+            Campaigns.ClickBannerOption();
+            Campaigns.ClickPopUpBannerCancelButton();
+            Campaigns.EnterCampaignName(expectedCampaignName);
+            Campaigns.ScrollDownToDraftButton();
+            Campaigns.ClickDraftButton();
+            string actualStatus = Campaigns.GetStatusText();
+            string actualCampaignName = Campaigns.GetCampaignText();
+
+            Assert.AreEqual(expectedCampaignName, actualCampaignName);
+            Assert.AreEqual(expectedStatus, actualStatus);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.CloseDriver();
+        }
     }
 }
